c716fe1fb46bce1d4e76798784bf74260385fb45
Martin Bonnin <martin.bonnin@dailymotion.com>
2019-04-16 15:44:38 +0200

    update build.gradle.kts
    * factor the kotlin version in build.gradle.kts
    * retrieve the AppEngine extension dynamically
    * add moshi
    * use fixed versions
    * remove jcenter

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,2 @@
 rootProject.name = "android-makers"
 
-val kotlinVersion: String by settings
-
-pluginManagement {
-    resolutionStrategy {
-        eachPlugin {
-            when (requested.id.id) {
-                "org.jetbrains.kotlin.jvm" -> useVersion(kotlinVersion)
-            }
-        }
-    }
-}
-
